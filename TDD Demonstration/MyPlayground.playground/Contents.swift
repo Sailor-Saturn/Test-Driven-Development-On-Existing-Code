3855b75e4b90cc21c5be99aa74e1723a8121e3f4
Sailor-Saturn <46728174+Sailor-Saturn@users.noreply.github.com>
2024-03-24 18:48:14 +0000

    Sample: Add the amount as constant

---
+++ TDD Demonstration/MyPlayground.playground/Contents.swift
@@ -0,0 +1,21 @@
+import XCTest
+
+final class Dollar {
+    var amount: Int = 10
+    
+    init(_ amount: Int) {
+    }
+    
+    func times(_ otherAmount: Int) {
+    }
+}
+
+final class TDDPlaygroundSample: XCTestCase {
+    func test_multiplication() {
+        let five = Dollar(5)
+        five.times(2)
+        XCTAssertEqual(10, five.amount)
+    }
+}
+
+TDDPlaygroundSample.defaultTestSuite.run()
